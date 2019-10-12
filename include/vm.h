8f1f8f5c213b959ffbffb2ca385f5b3ce6c36a68
gfredtech <gfredtech@gmail.com>
2019-10-12 15:34:43 +0000

    Babysteps for VM implementation

---
+++ include/vm.h
@@ -0,0 +1,20 @@
+#ifndef slang_vm_h
+#define slang_vm_h
+
+#include "chunk.h"
+typedef struct {
+  Chunk *chunk;
+  uint8_t *ip;
+} VM;
+
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk *chunk);
+
+#endif /* slang_vm_h */
