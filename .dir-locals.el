b4a85d443a2a03539823ba2fe830328d18a132e8
gfredtech <gfredtech@gmail.com>
2019-10-12 16:54:10 +0000

    Added cmake/helm project config

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cmake-ide-project-dir . "~/Slang")
+         (cmake-ide-build-dir . "~/Slang/build")
+         (helm-make-build-dir . "build")
+         (helm-ctest-dir . "~/Slang/build/"))))
