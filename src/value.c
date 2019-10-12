d7b30cd3da8019b3a3522792e144277884cb5ebb
gfredtech <gfredtech@gmail.com>
2019-10-12 15:05:19 +0000

    Added line number information for debugging bytecode

--- src/value.c
+++ src/value.c
@@ -3,7 +3,7 @@
 #include "memory.h"
 #include "value.h"
 
-void initValuArray(ValueArray *array) {
+void initValueArray(ValueArray *array) {
   array->values = NULL;
   array->capacity = 0;
   array->count = 0;
