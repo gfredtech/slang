19964516ef9e89ed0bed6ca230d55a68e50dc741
gfredtech <gfredtech@gmail.com>
2019-10-13 14:23:50 +0000

    Equality/coparison expressions

--- src/value.c
+++ src/value.c
@@ -39,3 +39,16 @@ void printValue(Value value) {
     break;
   }
 }
+
+bool valuesEqual(Value a, Value b) {
+  if (a.type != b.type) return false;
+
+  switch (a.type) {
+  case VAL_BOOL:
+    return AS_BOOL(a) == AS_BOOL(b);
+  case VAL_NUMBER:
+    return AS_NUMBER(a) == AS_NUMBER(b);
+  case VAL_NIL:
+    return true;
+  }
+}
