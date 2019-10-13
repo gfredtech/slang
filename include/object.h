bac5573be50098c2f45f6dc666893fee763f6f66
gfredtech <gfredtech@gmail.com>
2019-10-13 15:45:19 +0000

    String implementation

--- include/object.h
+++ include/object.h
@@ -10,7 +10,7 @@
 #define IS_STRING(value)     isObjType(value, OBJ_STRING)
 
 #define AS_STRING(value)     ((ObjString *)AS_OBJ(value))
-#define AS_CSTRING(value)    ((ObjString *)AS_OBJ(value)->chars)
+#define AS_CSTRING(value)    (((ObjString *)AS_OBJ(value))->chars)
 // clang-format on
 
 typedef enum {
@@ -19,6 +19,7 @@ typedef enum {
 
 struct sObj {
   ObjType type;
+  struct sObj *next;
 };
 
 struct sObjString {
@@ -26,6 +27,10 @@ struct sObjString {
   int length;
   char *chars;
 };
+
+ObjString *takeString(char *chars, int length);
+ObjString *copyString(const char *chars, int length);
+void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
   return IS_OBJ(value) && AS_OBJ(value)->type == type;
